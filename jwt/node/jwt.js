8ef8813090f5fda736e06d42da83cb89aab2a0ce
liuhaozhe <l19517863@163.com>
2019-10-07 23:39:07 +0800

    jwt-node

---
+++ jwt/node/jwt.js
@@ -0,0 +1,54 @@
+var fs = require('fs');
+const jwt = require('jsonwebtoken');
+
+/* =============================HS256========================================= */
+var token = jwt.sign({ foo: 'bar' }, 'pwd'); // 签名
+// 验证签名1 同步
+try {
+    var decoded = jwt.verify(token, 'wrong-secret');
+    console.log(decoded);
+  } catch(err) {
+      console.log('同步验签失败');
+  }
+
+// 验证签名2 异步
+jwt.verify(token, 'pwd', function(err, decoded) {
+    if(!err){
+    console.log(decoded.foo) // bar
+    }else{
+        console.log('异步验签失败',err);
+    }
+  });
+
+
+/* ================================== RS256 ======================================= */
+/* 
+  选择当前目录 生成 密匙对
+  ssh-keygen -m PEM -t rsa -b 2048 -f private.key
+  openssl rsa -in private.key -pubout -outform PEM -out public.key
+ */
+
+
+var private = fs.readFileSync('private.key');  // get private key
+var public = fs.readFileSync('public.key');  // get private key
+var token = jwt.sign({ foo: 'bar' }, private, { algorithm: 'RS256' });
+console.log(token);
+
+// // 异步签名
+// jwt.sign({ foo: 'bar' }, privateKey, { algorithm: 'RS256' }, function (err, token) {
+//   console.log(token);
+// });
+
+jwt.verify(token, public, function (err, decoded) {
+  if (err) {
+    console.log('验签失败');
+  } else {
+    console.log(decoded);
+  }
+});
+/* 
+token 本身没有加密的,  任何人都可以解码出,载体内容, 所以不要将秘密写进 载体里.  jwt 只是把载体内容 转码, 便于传输, 保存, 并做了一个签名, 可以验证篡改问题.   
+
+jsonwebtokoen npm 官方示例.
+LINK : https://www.npmjs.com/package/jsonwebtoken
+*/
---
+++ jwt/readme.md
@@ -0,0 +1,14 @@
+# jwt
+
+1. 生成私钥
+ssh-keygen -t rsa -b 2048 -f private.key  //  可能会是新的格式  BEGIN OPENSSH PRIVATE KEY
+ssh-keygen -m PEM -t rsa -b 2048 -f private.key // -m 指定格式  BEGIN RSA PRIVATE KEY , jsonwebtoken 需要这个格式的
+
+2. 生成公钥
+openssl rsa -in private.key -pubout -outform PEM -out public.key
+
+
+# 参考
+
+1. openssl和ssh-keygen的区别
+https://www.cnblogs.com/pixy/p/4722381.html
