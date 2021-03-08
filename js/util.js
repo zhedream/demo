dec7a1a103920b9907aea8b7650c033e7345d1d3
liuhaozhe <l19517863@163.com>
2021-03-08 09:31:49 +0800

    2021年3月5日

--- js/util.js
+++ js/util.js
@@ -40,3 +40,24 @@ export const mbLen = (str) => {
     }
     return len;
 }
+
+/**
+ * 去掉首尾两端的空格
+ * @param {*} str 
+ */
+export function trim(str) {
+    return str.replace(/(^\s*)|(\s*$)/g, "");
+}
+
+
+/**
+ * 返回第几列
+ * @param {*} count 当前第几个
+ * @param {*} columnCount 一行几个
+ * @return {number} 1 <= curColumn <= columnCount
+ */
+function whichColumn(count, columnCount) {
+  const remainder = (count - 1) % columnCount; // 下标的余数
+  const restCount = columnCount - remainder - 1; // 剩余的个数
+  return columnCount - restCount; // 第几个
+}
