24e9629f6fe52859de0bdc01a70b8d6716c37d8e
liuhaozhe <l19517863@163.com>
2021-04-09 17:50:45 +0800

    Update pattern.js

--- js/pattern.js
+++ js/pattern.js
@@ -15,3 +15,18 @@ console.log(p);
 
 const a = str.replace(pattern2);
 console.log(a);
+
+// 验证手机号
+
+var pattern = /^1[34578]\d{9}$/;
+
+// 验证座机号
+var pattern = /[0-9]{3,4}[-][0-9]{8}/;
+var pattern = /\d{3,4}[-]\d{8}/;
+
+// 手机或座机
+var pattern = /^1[34578]\d{9}$|^[0-9]{3,4}[-][0-9]{8}$/;
+
+// 密码强度: 对应的验证规则是：密码中必须包含字母、数字、特称字符，至少8个字符，最多30个字符。
+var regex = new RegExp('(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[^a-zA-Z0-9]).{8,30}');
+console.log(regex.test('a123456-'));
